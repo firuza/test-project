ee44009f0b1e958e58d2fbf629b2fc27f1af3940
Firuza Aibara <firya87@gmail.com>
2022-05-01 22:55:18 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+bool testFunction() {
+	bool mano=false;
+
+	for (int i=0; i<no_circles; i++) {
+		if (sqrt(((x-a[i])*(x-a[i]))+((y-b[i])*(y-b[i])))<20) {
+			mano=true;
+			break;
+		}
+	}
+	return mano;//we are returning the true when it is inside of the snake @@@@@@@@    
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
