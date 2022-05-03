5455d0c8140b06af9d4db87790c8ba420bce8ca5
firuza <firya87@gmail.com>
2022-05-03 18:56:51 +0530

    remove unwanted code

--- hello.cpp
+++ hello.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-bool testFunction() {
-	bool mano=false;
+// bool testFunction() {
+// 	bool mano=false;
 
-	for (int i=0; i<no_circles; i++) {
-		if (sqrt(((x-a[i])*(x-a[i]))+((y-b[i])*(y-b[i])))<20) {
-			mano=true;
-			break;
-		}
-	}
-	return mano;//we are returning the true when it is inside of the snake @@@@@@@@    
-}
+// 	for (int i=0; i<no_circles; i++) {
+// 		if (sqrt(((x-a[i])*(x-a[i]))+((y-b[i])*(y-b[i])))<20) {
+// 			mano=true;
+// 			break;
+// 		}
+// 	}
+// 	return mano;//we are returning the true when it is inside of the snake @@@@@@@@    
+// }
 
 int main()
 {
